afb664e302016369935a47e5577ef331086e2337
shamin26 <mostafa.yeasar@bjitgroup.com>
2023-09-19 16:37:52 +0600

    [RnD-EasyPurchase-iOS][477785]Utilize Apple's StoreKit framework to handle the presentation of subscription options to users
    - Implemented a struct to represent the result of a product retrieval operation.
    - Implemented a method to create and initiate a product retrieval request
    - Implemented a method to fetch product information for a set of product IDs using necessaray callback responses.
    
    Ref: https://redmine.bjitgroup.com/redmine/issues/477785

---
+++ Sources/EasyPurchase/Models/Products.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by BJIT on 19/9/23.
+//
+import Foundation
+import StoreKit
+
+// A struct to represent the result of a product retrieval operation.
+struct Products {
+    // A set of retrieved products (SKProduct) if the operation was successful,
+    // otherwise, it will be nil.
+    let retrievedProducts: Set<SKProduct>?
+
+    // A set of invalid product IDs, which couldn't be retrieved, if any.
+    // If the operation was successful for all products, this will be nil.
+    let invalidProductIDs: Set<String>?
+
+    // An error object that holds any errors encountered during the retrieval
+    // operation. If the operation was successful, this will be nil.
+    let error: Error?
+}
+
+
---
+++ Sources/EasyPurchase/ProductInfoController.swift
@@ -0,0 +1,36 @@
+//
+//  ProductInfoController.swift
+//
+//
+//  Created by BJIT on 19/9/23.
+//
+import Foundation
+import StoreKit
+
+// A protocol for handling product information retrieval.
+protocol ProductsInfoHandler {
+    // A method to fetch product information for a set of product IDs.
+    // - Parameters:
+    //   - productIds: A set of product IDs to fetch information for.
+    //   - completion: A closure that takes a `Products` object as its parameter
+    //     and is called when the product information retrieval is completed.
+    // - Returns: An `InAppProductRequest` object that can be used to start the request.
+    func fetchProductsInfo(_ productIds: Set<String>, completion: @escaping (Products) -> Void) -> InAppProductRequest
+}
+
+// A class that conforms to the `ProductsInfoHandler` protocol.
+class ProductInfoController: NSObject, ProductsInfoHandler {
+    // An instance of `InAppProductFetcherBuilder` used to build product retrieval requests.
+    var fetchProductBuilder = InAppProductFetcherBuilder()
+
+    init(fetchProductBuilder: InAppProductFetcherBuilder = InAppProductFetcherBuilder()) {
+        self.fetchProductBuilder = fetchProductBuilder
+    }
+
+    // Implementation of the `fetchProductsInfo` method as required by the protocol.
+    // It initiates the product retrieval request and returns an `InAppProductRequest` object.
+    func fetchProductsInfo(_ productIds: Set<String>, completion: @escaping (Products) -> Void) -> InAppProductRequest {
+        // Use the builder to create the request, passing in the product IDs and completion closure.
+        return fetchProductBuilder.request(productIds: productIds, callback: completion)
+    }
+}
