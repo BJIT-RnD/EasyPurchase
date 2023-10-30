56b9623c497161c7d060c599b52544fb0f0ddc9e
Reashed Zamil <reashed.zamil@bjitgroup.com>
2023-10-30 12:41:31 +0600

    [IAP][EP] Restructure demo project according to the latest interface builder and EP doc - Restructure code according to the EP doc - Remove unnecessary codes - Remove UnitTest from the Demo Project - Handle all warnings

---
+++ EasyPurchaseDemo/EasyPurchaseDemo/Classes/MainViewController/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  EasyPurchaseDemo
+//
+//  Created by rex on 30/10/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+        self.title = "Main View"
+    }
+}
